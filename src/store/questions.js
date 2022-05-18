90188afc412cab18e54fe4f9e8465a28a1ad30ac
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-18 17:45:17 +0200

    add new questions

--- src/store/questions.js
+++ src/store/questions.js
@@ -1,67 +1,331 @@
 const questions = [
   {
     id: 1,
-    question: "o?",
+    question: "Fiore sei gay?",
     answers: [
       {
-        text: "k",
+        text: "si",
         correct: false,
       },
       {
-        text: "ko",
+        text: "obv",
         correct: false,
       },
       {
-        text: "km",
+        text: "maaaaa",
         correct: false,
       },
       {
-        text: "ok",
+        text: "kkk",
         correct: true,
       },
     ],
   },
   {
     id: 2,
-    question: "tes?",
+    question: "AMAAAAA?",
     answers: [
       {
-        text: "k",
+        text: "AAAAAAAA",
         correct: false,
       },
       {
-        text: "ko",
-        correct: false,
-      },
-      {
-        text: "km",
-        correct: false,
-      },
-      {
-        text: "ok",
+        text: "MMAAMAMAMAMAMAMAUTO(?)",
         correct: true,
+      },
+      {
+        text: "MAZZATI",
+        correct: false,
+      },
+      {
+        text: "AAAH?",
+        correct: false,
       },
     ],
   },
   {
     id: 3,
-    question: "dem?",
+    question: "Nome e cognome",
     answers: [
       {
-        text: "k",
+        text: "domenico",
         correct: false,
       },
       {
-        text: "ko",
+        text: "vincenzo domenico",
         correct: false,
       },
       {
-        text: "km",
+        text: "galliano domenico",
         correct: false,
       },
       {
-        text: "ok",
+        text: "dome domenico",
         correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "8 + 8",
+    answers: [
+      {
+        text: "88",
+        correct: false,
+      },
+      {
+        text: "8",
+        correct: false,
+      },
+      {
+        text: "MABASTAAAAAAA",
+        correct: true,
+      },
+      {
+        text: "NODAISIAMOAMICI",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "dio",
+    answers: [
+      {
+        text: "negro",
+        correct: true,
+      },
+      {
+        text: "automobilistico",
+        correct: false,
+      },
+      {
+        text: "stracchino",
+        correct: false,
+      },
+      {
+        text: "pera",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Porto vs.",
+    answers: [
+      {
+        text: "dio",
+        correct: false,
+      },
+      {
+        text: "napule",
+        correct: true,
+      },
+      {
+        text: "real madrid",
+        correct: false,
+      },
+      {
+        text: "inter",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "TODO",
+    answers: [
+      {
+        text: "TODO",
+        correct: true,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
+      },
+      {
+        text: "TODO",
+        correct: false,
       },
     ],
   },
